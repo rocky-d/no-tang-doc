b7719418e70ed6fc18aa7b08111a713065f43963
ShiDu <1364136725@qq.com>
2025-10-09 19:35:57 +0800

    change LogController api path

--- no-tang-doc-core/src/main/java/com/ntdoc/notangdoccore/controller/LogController.java
+++ no-tang-doc-core/src/main/java/com/ntdoc/notangdoccore/controller/LogController.java
@@ -58,7 +58,7 @@ public class LogController {
     * 获取当前用户的日志条数
     * 可以设置每周或者每月
     */
-    @GetMapping("/list")
+    @GetMapping("/count")
     public ResponseEntity<Map<String,Long>> getLogsCount(@AuthenticationPrincipal Jwt jwt,
                                                          @RequestParam(defaultValue = "week")
                                                          @Pattern(regexp = "^(week|month)$",message = "period can only be week or month")
